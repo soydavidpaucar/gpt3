36c30166391b835e6e9a3f7f0267d9357bac0325
David Paucar <soydavidpaucar@gmail.com>
2021-11-13 16:33:20 -0500

    :art: brand structure
    created brand structure with image imports

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,21 @@
 import './brand.css';
+import {dropbox, google, shopify, slack} from './imports';
 
 const Brand = () => {
 	return (
-		<div>
-			Brand
+		<div className="brand section__padding">
+			<div>
+				<img src={google} alt="Google Image" />
+			</div>
+			<div>
+				<img src={slack} alt="Slack Image" />
+			</div>
+			<div>
+				<img src={dropbox} alt="Dropbox Image" />
+			</div>
+			<div>
+				<img src={shopify} alt="Shopify Image" />
+			</div>
 		</div>
 	);
 };
