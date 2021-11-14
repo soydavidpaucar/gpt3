9e56fcdd56ac22bb30ade55fda074e9a8d62d13c
David Paucar <soydavidpaucar@gmail.com>
2021-11-14 11:55:31 -0500

    :art: atlassian logo
    created import for atlassian logo

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -1,11 +1,13 @@
+import atlassian from '../../assets/atlassian.png';
 import google from '../../assets/google.png';
-import slack from '../../assets/slack.png';
 import dropbox from '../../assets/dropbox.png';
 import shopify from '../../assets/shopify.png';
+import slack from '../../assets/slack.png';
 
 export {
+	atlassian,
 	google,
-	slack,
 	dropbox,
-	shopify
+	shopify,
+	slack
 };
