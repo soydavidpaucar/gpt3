badc260671ac31070dd94a2fbbe6e3eb3b9545ce
David Paucar <soydavidpaucar@gmail.com>
2021-11-27 18:37:27 -0500

    :art: cta structure
    created cta structure

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,8 +2,14 @@ import './cta.css';
 
 const CTA = () => {
 	return (
-		<div>
-			CTA
+		<div className="cta">
+			<div className="cta__content">
+				<p>Request Early Access to Get Started</p>
+				<h3>Register today & start exploring the endless possiblities.</h3>
+			</div>
+			<div className="cta__button">
+				<button type="button">Get Started</button>
+			</div>
 		</div>
 	);
 };
