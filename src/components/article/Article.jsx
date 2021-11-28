2f826ee2a56a53959a4a004b4fa91c7a0401cde9
David Paucar <soydavidpaucar@gmail.com>
2021-11-27 19:11:08 -0500

    :art: article structure
    created article structure

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,18 @@
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl, date, title}) => {
 	return (
-		<div>
-			Article
+		<div className="article">
+			<div className="article__image">
+				<img src={imgUrl} alt="Blog" />
+			</div>
+			<div className="article__content">
+				<div>
+					<p>{date}</p>
+					<h3>{title}</h3>
+				</div>
+				<p>Read Full Article</p>
+			</div>
 		</div>
 	);
 };
