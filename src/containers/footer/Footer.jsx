03732c9da4beb1aaff71ab218dc604e8459c82cb
David Paucar <soydavidpaucar@gmail.com>
2021-11-27 20:42:31 -0500

    :art: footer structure
    created footer structure

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,48 @@
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
 	return (
-		<div>
-			Footer
+		<div className="footer section--padding">
+			<div className="footer__heading">
+				<h1 className="gradient-text">Do you want to step in to the future before others</h1>
+			</div>
+			<div className="footer__button">
+				<p>Request Early Access</p>
+			</div>
+			
+			<div className="footer__links">
+				<div className="footer__links__logo">
+					<img src={gpt3Logo} alt="Logo" />
+					<p>Crechterwoord K12 1B2 DK Alknjkcb, All Rights Reserved</p>
+				</div>
+				
+				<div className="footer__links__div">
+					<h4>Links</h4>
+					<p>Overons</p>
+					<p>Social Media</p>
+					<p>Counters</p>
+					<p>Contact</p>
+				</div>
+				
+				<div className="footer__links__div">
+					<h4>Company</h4>
+					<p>Terms & Conditions</p>
+					<p>Privacy Policy</p>
+					<p>Contact</p>
+				</div>
+				
+				<div className="footer__links__div">
+					<h4>Get in touch</h4>
+					<p>Crechterwoord K12 1B2 DK Alknjkcb</p>
+					<p>085-132567</p>
+					<p>info@payme.net</p>
+				</div>
+			</div>
+			
+			<div className="footer__copyright">
+				<p>© 2021 GPT-3. All rights reserved.</p>
+			</div>
 		</div>
 	);
 };
