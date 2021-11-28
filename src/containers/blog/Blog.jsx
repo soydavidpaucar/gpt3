5d38704f91e173231ecdfc0476459bd6fbe4632b
David Paucar <soydavidpaucar@gmail.com>
2021-11-27 19:11:56 -0500

    :art: blog structure
    created blog structure

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,30 @@
 import './blog.css';
+import {Article} from '../../components';
+import {blog01, blog02, blog03, blog04, blog05} from './imports';
 
 const Blog = () => {
 	return (
-		<div>
-			Blog
+		<div className="blog section--padding" id="blog">
+			<div className="blog__heading">
+				<h1 className="gradient-text">A lot is happening, We are blogging about it.</h1>
+			</div>
+			
+			<div className="blog__container">
+				<div className="blog__container__groupA">
+					<Article imgUrl={blog01} date="Sep 6, 2021"
+					         title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+				</div>
+				<div className="blog__container__groupB">
+					<Article imgUrl={blog02} date="Sep 6, 2021"
+					         title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+					<Article imgUrl={blog03} date="Sep 6, 2021"
+					         title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+					<Article imgUrl={blog04} date="Sep 6, 2021"
+					         title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+					<Article imgUrl={blog05} date="Sep 6, 2021"
+					         title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+				</div>
+			</div>
 		</div>
 	);
 };
